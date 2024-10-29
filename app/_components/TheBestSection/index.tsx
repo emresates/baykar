25d1d35f52738057bba0e9d73efe5bbebace6a9f
Secenory <emresates@outlook.com>
2024-10-29 18:26:44 +0300

    best section responsive

--- app/_components/TheBestSection/index.tsx
+++ app/_components/TheBestSection/index.tsx
@@ -26,28 +26,35 @@ const TheBest = () => {
   ];
 
   return (
-    <div className="relative bg-[#0F172A] p-20 text-white">
+    <div className="relative bg-[#0F172A] px-4 py-12 text-white desktop:p-20">
       <Image
         src="/images/best/bg.png"
         alt="hero"
         width={500}
         height={500}
-        className="absolute bottom-14 left-10 z-0 h-auto w-[calc(100%-80px)] object-contain"
+        className="absolute bottom-14 left-10 z-0 hidden h-auto w-[calc(100%-80px)] object-contain desktop:block"
       />
-      <div className="space-y-20">
-        <div className="flex items-center justify-between">
-          <h1 className="text-heading-2 font-extrabold">
+      <Image
+        src="/images/best/bgmobile.png"
+        alt="hero"
+        width={500}
+        height={500}
+        className="absolute top-60 left-0 z-0 h-[calc(100%-240px)] w-full object-contain desktop:hidden"
+      />
+      <div className="space-y-8 desktop:space-y-20">
+        <div className="flex flex-col items-center justify-between gap-y-8 desktop:flex-row">
+          <h1 className="text-center text-heading-5 font-extrabold desktop:text-left desktop:text-heading-2">
             The best of the best
           </h1>
           <Button
-            className="text-xxl h-[64px] w-[233px] p-0 font-bold hover:shadow-white/40"
+            className="h-[64px] w-[233px] p-0 text-xxl font-bold hover:shadow-white/40"
             size="custom"
             color="white"
           >
             Sing up now
           </Button>
         </div>
-        <div className="flex items-center justify-between gap-12">
+        <div className="flex flex-col items-center justify-between gap-12 desktop:flex-row">
           {data?.map((item, index) => <Card key={index} {...item} />)}
         </div>
       </div>
